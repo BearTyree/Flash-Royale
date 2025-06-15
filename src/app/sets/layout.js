116531f35743aef2ab7601d6fdb0f4cc9b72c475
BearTyree <beartyree@blueridgegrizzly.com>
2025-06-15 17:41:24 -0400

    fix merge conflicts in layout file of sets

--- src/app/sets/layout.js
+++ src/app/sets/layout.js
@@ -1,11 +1,7 @@
 import Header from "@/components/Header";
 import styles from "@/styles/create.module.css";
 
-<<<<<<<< HEAD:src/app/(flashcards)/layout.js
 export default function FlashLayout({ children }) {
-========
-export default function SetsLayout({ children }) {
->>>>>>>> 6d5f72ee2dcb71d518c2ef4fe55e3ba23bc6e232:src/app/sets/layout.js
   return (
     <>
       <div id={styles.createContent}>
