1ccfd276a2c46d177dd09bbdb87c05eef28a7de5
BearTyree <beartyree@blueridgegrizzly.com>
2025-06-21 15:25:01 -0400

    reorganize stuff so I can access the room code from play

--- src/app/(game)/room/[code]/page.js
+++ src/app/(game)/room/[code]/page.js
@@ -5,5 +5,20 @@ export default async function Room() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value || "";
 
-  return <RoomClient token={token} />;
+  const db = await getDbAsync();
+  const username = await authenticated();
+
+  if (!username) {
+    return <div>Error</div>;
+  }
+
+  const user = await db.query.usersTable.findFirst({
+    where: (users, { eq }) => eq(users.username, username),
+  });
+
+  const cardSets = await db.query.cardSetsTable.findMany({
+    where: (cardSets, { eq }) => eq(cardSets.ownerId, user.id),
+  });
+
+  return <RoomClient cardSets={cardSets} token={token} />;
 }
--- src/app/(game)/room/[code]/room.js
+++ src/app/(game)/room/[code]/room.js
@@ -1,31 +1,62 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function RoomClient({ token }) {
+export default function RoomClient({ token, cardSets }) {
+  const [owner, setOwner] = useState(null);
+  const [name, setName] = useState(null);
+  const [ownRoom, setOwnRoom] = useState(false);
+
   const params = useParams();
   const code = params.code;
+  const wsRef = useRef(null);
 
-  const ws = new WebSocket(
-    (process.env.NEXT_PUBLIC_WS_SERVER || "") + "/room" + `/${code}`
-  );
+  useEffect(() => {
+    const ws = new WebSocket(
+      (process.env.NEXT_PUBLIC_WS_SERVER || "") + "/room" + `/${code}`
+    );
 
-  ws.addEventListener("open", (event) => {
-    ws.send(JSON.stringify({ event: "join", token }));
-  });
+    wsRef.current = ws;
 
-  ws.addEventListener("message", (message) => {
-    const { event } = JSON.parse(message.data);
+    ws.addEventListener("open", (event) => {
+      ws.send(JSON.stringify({ event: "join", token }));
+    });
 
-    switch (event) {
-      case "roomCode": {
-        const { code } = JSON.parse(message.data);
-        router.push(`/room/${code}`);
-        break;
+    ws.addEventListener("message", (message) => {
+      const { event } = JSON.parse(message.data);
+
+      switch (event) {
+        case "details": {
+          const { owner: ownerData, name: nameData } = JSON.parse(message.data);
+
+          setOwner(ownerData);
+          setName(nameData);
+          break;
+        }
+        case "owner": {
+          setOwnRoom(true);
+          break;
+        }
       }
-    }
-  });
+    });
+  }, [code, token]);
 
-  return <>Room {}</>;
+  return (
+    <>
+      {name}
+      <br />
+      {ownRoom && (
+        <div>
+          {cardSets.map((set) => (
+            <div key={set.id}>
+              {JSON.parse(set.cards).name}
+              <br />
+              <button>Use Set</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
